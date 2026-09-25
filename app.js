e93f6a81956721d85f9488b553c70cf57e16a21b
The Kyle Wells <github@kylewells.com>
2026-09-25 01:20:38 -0400

    mobile

--- app.js
+++ app.js
@@ -100,11 +100,11 @@ function formatMs(ms) {
 
 function escapeHtml(value) {
   return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 function updateStatus(message) {
